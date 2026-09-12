0c1fb75721d9203722d50e5445ac1df993f9ef09
slx23m <slman14422003@gmail.com>
2026-09-12 08:40:24 +0300

    Add files via upload

--- app/src/main/java/com/shopmanager/app/ui/splash/SplashScreen.kt
+++ app/src/main/java/com/shopmanager/app/ui/splash/SplashScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.shopmanager.app.ui.common.BrandGradient
 import com.shopmanager.app.ui.common.LiquidGlassGlow
