ca3bce02031384fe65ca9c0453198ba563a8ce69
slx23m <slman14422003@gmail.com>
2026-09-23 14:16:22 +0300

    Add padding import to MainActivity.kt

--- app/src/main/java/com/shopmanager/app/MainActivity.kt
+++ app/src/main/java/com/shopmanager/app/MainActivity.kt
@@ -31,6 +31,7 @@ import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.windowInsetsPadding
